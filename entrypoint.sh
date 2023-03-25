f94a0420a859ae79bd640ea442f7814418e6976c
Marvin Kweyu <mkweyu1@gmail.com>
2023-03-25 14:19:02 +0300

    chore: fix: entrypoint sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,17 @@
+#!/bin/sh
 
-#! /bin/sh
-
-echo "starting project"
-set -e
-
-# ensure postgres is healthy before applying migrationss
-
-if ["$DATABASE" = "postgres"]
+if [ "$DATABASE" = "postgres" ]
 then
-    echo " waiting for postgres ..."
+    echo "Waiting for postgres..."
 
-    while !nc -z $SQL_HOST $SQL_PORT; do
-        sleep 0.1
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
     done
 
     echo "PostgreSQL started"
-
 fi
 
+python manage.py flush --no-input
+python manage.py migrate
 
 exec "$@"
